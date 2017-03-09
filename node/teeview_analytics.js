350509fbe3f6b44fb79b2eef87caef09ce6c73d1
Pedro Atencio <aukintux@gmail.com>
2017-03-09 17:32:23 -0500

    Created writeCampaignToFirebase Function. Translated campaignsUpdater Function. Missing Firebase DB Operations.

--- node/teeview_analytics.js
+++ node/teeview_analytics.js
@@ -2,6 +2,7 @@
 var request = require('tinyreq');
 var cheerio = require("cheerio");
 var admin = require("firebase-admin");
+var fs = require("fs");
 
 // Authenticate Server to Firebase
 var serviceAccount = require("./teeview-movement-sensor-key.json");
@@ -23,7 +24,17 @@ var refCampaigns = db.ref("draw_data");
 var page = 1;
 var continue_scraping = true;
 var teeview_data = [];
-var config_data = require("./config.json");
+var config = require("./config.json");
+
+// Save config object into config file
+function update_config (latest_campaign_link, last_query_timestamp) {
+    config.latest_campaign_link = latest_campaign_link;
+    config.last_query_timestamp = last_query_timestamp;
+    fs.writeFile("./config.json", JSON.stringify(config), (err) => {
+        if (err) throw err;
+        console.log("Saved config.json");
+    });
+}
 
 // Define recursive teeviewScraper
 function teeviewScraper () {
@@ -39,7 +50,7 @@ function teeviewScraper () {
     		var campaign_url = $(this).find("h3 a").attr("href");
     		var campaign_time_ago = $(this).find("p.text-muted small").text();
     		// If reached campaigns added within the last day or latest campaign queried break each loop
-    		if (campaign_time_ago.indexOf("day") !== -1 || config_data["LATEST_CAMPAIGN_LINK"] === campaign_url) {
+    		if (campaign_time_ago.indexOf("day") !== -1 || config["LATEST_CAMPAIGN_LINK"] === campaign_url) {
     			continue_scraping = false;
     			return false;
     		}
@@ -51,7 +62,9 @@ function teeviewScraper () {
     		page++;
     		teeviewScraper();
     	} else {
+             console.log("ending teeview_scraper.");
     		// It ended scraping. Go on to the next phase i.e. campaignsUpdater.
+            campaignsUpdater();
     	}
 	});
 }
@@ -63,8 +76,49 @@ teeviewScraper();
 // and for each of them request the Teespring campaign in order to find out if it is relevant
 // or not based on the fact if the campaign reports sales data or not. If it does it adds the
 // new campaign to the respective database table.
-function campaignsUpdater (teeviewData) {
-	// >> Code goes here
+
+function writeCampaignToFirebase (i) {
+    console.log("writeCampaignToFirebase::", i);
+    if (i < teeview_data.length) {
+        var campaign_url = teeview_data[i];
+        request(campaign_url, function (err, body) {
+            // If error return
+            if (err) { return console.log("Error in request @ writeCampaignToFirebase: ", err); }
+            // Parse body into "$" variable
+            var $ = cheerio.load(body); 
+            // If the html element where sales data is reported exit continue
+            var sales_data_html_el = $(".persistent_timer__order_count");
+            if (sales_data_html_el.length > 0) {
+                var sales_data_text = sales_data_html_el.text().toLowerCase();
+                if (sales_data_text.indexOf("only") !== -1 || sales_data_text.indexOf("sold") !== -1) {
+                    // Write campaign to firebase
+                    console.log("write into Firebase: ", campaign_url);
+                    writeCampaignToFirebase(i+1);
+                    // campaigns.append((campaign_url, html.select(".campaign__name")[0].getText(), "https:" + html.select(".image_stack__image")[0]["src"]))
+                } else {
+                    // Make next request
+                    writeCampaignToFirebase(i+1);
+                }
+            } else {
+                // Make next request
+                writeCampaignToFirebase(i+1);
+            }
+        });
+    } else {
+        console.log("ending campaigns_updater.")
+        // Ended campaigns updater. Go on to the next step. Sales data updater.
+    }
+}
+
+
+function campaignsUpdater () {
+	console.log("starting capaigns_updater...");
+    // Update the JSON config file if there is new data on teeview
+    if (teeview_data) {
+        update_config(teeview_data[0], Math.floor(Date.now() / 1000));
+    }
+    // Loop through each link and check if it reports sales data on Teespring campaign page
+    writeCampaignToFirebase(0);
 }
 
 // This function will go through the database table that stores the Teespring campaigns that
